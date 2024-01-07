850d728b40d9585071eedc2b6b0b1ae67e4dc67b
krava101 <kravalex2004@gmail.com>
2024-01-07 16:38:14 +0200

    Added an adaptive for tablets

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const openMenuBtn = document.querySelector(".burger-btn");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".backdrop");
 const selService = document.querySelector(".form-selected-service");
 const servicesList = document.querySelector(".form-services-list");
 
@@ -8,17 +8,17 @@ const userForm = document.querySelector(".contact-form");
 const userInfo = {};
 
 const menuOnClick = event => {
-  if (event.target.dataset.link) menu.classList.remove("menu-active");
+  if (event.target.dataset.link) menu.classList.remove("backdrop-active");
   menu.removeEventListener("click", menuOnClick);
 }
 
 const closeMenuOnClick = () => {
-  menu.classList.remove("menu-active");
+  menu.classList.remove("backdrop-active");
   closeMenuBtn.removeEventListener("click", closeMenuOnClick);
 }
 
 const openMenuOnClick = () => {
-  menu.classList.add("menu-active");
+  menu.classList.add("backdrop-active");
   closeMenuBtn.addEventListener("click", closeMenuOnClick);
   menu.addEventListener("click", menuOnClick);
 }
